4703154758c409fa1ba6145794604bc8602cb52e
Ilak0121 <s7515199@gmail.com>
2019-03-16 18:24:57 +0900

    file saving

--- sensors/python_code/get_current.py
+++ sensors/python_code/get_current.py
@@ -1,15 +1,16 @@
-import os
+import os.path
 import re
 import sys
-import argparse
 import time
+import argparse
+
 from ina219 import (INA219, DeviceRangeError)
 
 SHUNT_OHMS = 0.1
 
-DEBUG_MODE = 0
+DEBUG_MODE = 0 #save file or print monitor
 
-def sensing(save):
+def sensing(args):
     ina = INA219(SHUNT_OHMS) #need to accurate
     ina.configure()
 
@@ -20,6 +21,7 @@ def sensing(save):
 
     time_avg=0
     n=0
+    txt = ''
     while(True): #sensing process
         try:
             string = '%.3f'%time.time()
@@ -28,8 +30,13 @@ def sensing(save):
                 #time1 = time.time()
                 string += '%.4f'%ina.current()
                 #time2 = time.time()
-                if save == False:
+                #if args.save == False:
+                if DEBUG_MODE == 1:
                     print(string)
+                else:
+                    txt += string + '\n'  #overflow considering after experiment
+
+                    #'''for debugging'''
                     #diff = "i/o each load time is:"+str(time2-time1)
                     #time_avg += (time2-time1)
                     #n += 1
@@ -39,18 +46,24 @@ def sensing(save):
             print(e)
         except (KeyboardInterrupt,EOFError):
             #print("avg"+str(time_avg/n))
+            if DEBUG_MODE == 0:
+                path = './test.txt'
+                fd = open(path,'w')
+                fd.write('%s %s \n' % (args.timestamp, args.name))
+                fd.write(txt)
+                fd.close()
             sys.exit(1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Processing Sensing data with timestamp reservation and testcase naem saving')
 
-    parser.add_argument('-s', '--save', help='-s [True/False]; save or not', type=bool, default=False)
+    #parser.add_argument('-s', '--save', help='-s [True/False]; save or not', type=bool, default=False)
     parser.add_argument('-n', '--name', help='-n [TESTCASE_NAME]', required=True) 
     parser.add_argument('-t', '--timestamp', type=int, required=True, help= '-t [TIMESTAMP]')
 
     args = parser.parse_args()
     print(args)
 
-    sensing(args.save)
+    sensing(args) # gonna give parameter of timestamp following 2 second start
 
